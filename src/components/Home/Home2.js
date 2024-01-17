247e0741314807a6995cbb18a7216353940763e1
Vijay Shankar <Put your e-mail ID here>
2024-01-18 02:32:05 +0530

    added

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,28 +19,29 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming and involved in continuos
+              learning… 🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
                 <b className="purple">
                   {" "}
-                  Javascript, React, Node, Three,Forge, MongoDB, MySQL, AWS,
-                  System Design.{" "}
+                  Javascript, React.js, Node.js, Three.js,Forge, MongoDB, MySQL,
+                  AWS, System Design.{" "}
                 </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to <b className="purple">Blockchain.</b>
+                <b className="purple">Web Technologies and 3D Products </b> and
+                also in areas related to{" "}
+                <b className="purple">AEC/Finance/Energy.</b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple">MERN Stacks</b> and
               <i>
                 <b className="purple">
                   {" "}
